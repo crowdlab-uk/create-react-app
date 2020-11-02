903edad8f744484937a90270ab335bec31d15444
Toyin Ajayi <toyinajayi22@outlook.com>
2020-11-02 13:58:49 +0000

    add polyfills

---
+++ packages/react-scripts/config/polyfills.js
@@ -0,0 +1,76 @@
+'use strict';
+
+require('intersection-observer');
+
+require('core-js');
+
+if (typeof Promise === 'undefined') {
+  // Rejection tracking prevents a common issue where React gets into an
+  // inconsistent state due to an error, but it gets swallowed by a Promise,
+  // and the user has no idea what causes React's erratic future behavior.
+  require('promise/lib/rejection-tracking').enable();
+  window.Promise = require('promise/lib/es6-extensions.js');
+}
+
+// fetch() polyfill for making API calls.
+require('whatwg-fetch');
+
+// Object.assign() is commonly used with React.
+// It will use the native implementation if it's present and isn't buggy.
+Object.assign = require('object-assign');
+
+// In tests, polyfill requestAnimationFrame since jsdom doesn't provide it yet.
+// We don't polyfill it in the browser--this is user's responsibility.
+if (process.env.NODE_ENV === 'test') {
+  require('raf').polyfill(global);
+}
+
+// Polyfill for ParentNode.append()
+// https://developer.mozilla.org/en-US/docs/Web/API/ParentNode/append
+(function(arr) {
+  arr.forEach(function(item) {
+    if (item.hasOwnProperty('append')) {
+      return;
+    }
+    Object.defineProperty(item, 'append', {
+      configurable: true,
+      enumerable: true,
+      writable: true,
+      value: function append() {
+        var argArr = Array.prototype.slice.call(arguments),
+          docFrag = document.createDocumentFragment();
+
+        argArr.forEach(function(argItem) {
+          var isNode = argItem instanceof Node;
+          docFrag.appendChild(
+            isNode ? argItem : document.createTextNode(String(argItem))
+          );
+        });
+
+        this.appendChild(docFrag);
+      },
+    });
+  });
+})([Element.prototype, Document.prototype, DocumentFragment.prototype]);
+
+if (!document.elementsFromPoint) {
+  document.elementsFromPoint = elementsFromPoint;
+}
+
+function elementsFromPoint(x, y) {
+  var parents = [];
+  var parent = void 0;
+  do {
+    if (parent !== document.elementFromPoint(x, y)) {
+      parent = document.elementFromPoint(x, y);
+      parents.push(parent);
+      parent.style.pointerEvents = 'none';
+    } else {
+      parent = false;
+    }
+  } while (parent);
+  parents.forEach(function(parent) {
+    return (parent.style.pointerEvents = 'all');
+  });
+  return parents;
+}
